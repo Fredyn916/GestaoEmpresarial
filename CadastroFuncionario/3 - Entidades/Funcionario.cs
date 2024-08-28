357bc653326a49a8af8ae94a45379439cd3eda7f
Fred Marques <fredyn.curso@gmail.com>
2024-08-28 16:48:46 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroFuncionario/3 - Entidades/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoEmpresarial._3___Entidades
+{
+    public class Funcionario
+    {
+        int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+}
