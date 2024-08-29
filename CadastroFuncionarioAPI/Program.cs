c165aa5bd68747a7bbadb42fcf305f79ecc9db89
Fred Marques <fredyn.curso@gmail.com>
2024-08-29 17:06:56 -0300

    DB

--- CadastroFuncionarioAPI/Program.cs
+++ CadastroFuncionarioAPI/Program.cs
@@ -1,3 +1,5 @@
+using GestaoEmpresarial.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+InicializadorDB.Inicializar();
 
 var app = builder.Build();
 
