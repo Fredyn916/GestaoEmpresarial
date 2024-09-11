576383cca3a794d04c7b159d808f59d5930b4534
Fred Marques <fredyn.curso@gmail.com>
2024-09-11 14:43:47 -0300

    economia

--- CadastroFuncionario/3 - Entidades/DTO/FuncionarioDTO/CreateEconomiaDTO.cs
+++ CadastroFuncionario/3 - Entidades/DTO/FuncionarioDTO/CreateEconomiaDTO.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GestaoEmpresarial.Entidades
+namespace GestaoEmpresarial._3___Entidades.DTO.FuncionarioDTO
 {
-    public class Economia
+    public class CreateEconomiaDTO
     {
         [Required(ErrorMessage = "Compo Obrigatório 'DataCalculoFinanceiro' não preenchido")]
         public DateOnly DataCalculoFinanceiro { get; set; } // Data da Folha Financeira da Empresa
@@ -15,15 +15,11 @@ namespace GestaoEmpresarial.Entidades
         public double TotalBruto { get; set; } // Total valor arrecadado pela empresa
         [Required(ErrorMessage = "Compo Obrigatório 'TotalInvestimentos' não preenchido")]
         public double TotalInvestimentos { get; set; } // Total em aportes feitos por investidores 
-        [Required(ErrorMessage = "Compo Obrigatório 'TotalCapital' não preenchido")]
-        public double TotalCapital { get; set; }
         [Required(ErrorMessage = "Compo Obrigatório 'DespesasImovel' não preenchido")]
         public double DespesasImovel { get; set; } // Total em contas do Imóvel
         [Required(ErrorMessage = "Compo Obrigatório 'DespesasFuncionarios' não preenchido")]
         public double DespesasFuncionarios { get; set; } // Total em gastos gerais com funcionários
         [Required(ErrorMessage = "Compo Obrigatório 'DespesasServicos' não preenchido")]
         public double DespesasServicos { get; set; } // Total em gastos com projetos, trabalhos, serviços, etc
-        public double TotalDespesas { get; set; } // Total de gastos da empresa
-        public double CapitalResultado { get; set; } // Diferença entre valor arrecadado menos as despesas
     }
 }
--- CadastroFuncionarioAPI/Controllers/EconomiaController.cs
+++ CadastroFuncionarioAPI/Controllers/EconomiaController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GestaoEmpresarial.Entidades.DTO.FuncionarioDTO;
+using GestaoEmpresarial.Entidades;
+using GestaoEmpresarial.Repository.Data.Script;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestaoEmpresarialAPI.Controllers
 {
     public class EconomiaController : ControllerBase
     {
+        [HttpPost("AdicionarFolhaFinanceira")] // Rota (EndPoint)
+        public void AdicionarCidade(CreateFuncionarioDTO funcionarioToMap)
+        {
+            Funcionario funcionario = _Mapper.Map<Funcionario>(funcionarioToMap);
+            funcionario.Salario = FuncionarioScript.GetSalarioFromCargoId(funcionarioToMap.CargoId);
 
+            _Service.Adicionar(funcionario);
+        }
     }
 }
